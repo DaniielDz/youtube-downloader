79139bd656c18a74b091f84a0a5756824fa189a6
DaniielDz <daniel13cba2002@gmail.com>
2024-04-06 18:38:11 -0300

    Codigo anterior refactorizado, funciona perfecto

--- frontend/src/components/FormVideo.jsx
+++ frontend/src/components/FormVideo.jsx
@@ -6,96 +6,73 @@ const URLHOST = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000";
 
 function FormVideo() {
     const [videoLink, setVideoLink] = useState('');
-    const [url, setUrl] = useState('');
-    const [title, setTitle] = useState('');
-    const [loading, setLoading] = useState(false); // Nuevo estado para el indicador de carga
-    const [iframe, setIframe] = useState(''); // Nuevo estado para el indicador de carga
+    const [loading, setLoading] = useState(false);
+    const [mediaOptions, setMediaOptions] = useState({ videos: [], audios: [], iframe: '' });
 
-    const [videos, setVideos] = useState([]); // Utiliza useState para almacenar los videos
-    const [audios, setAudios] = useState([]); // Utiliza useState para almacenar los audios
-
-    const handleChange = e => { setVideoLink(e.target.value);}
+    const handleChange = e => setVideoLink(e.target.value);
 
     const handleSubmit = async e => {
         e.preventDefault();
-        setLoading(true); // Mostrar el indicador de carga al comenzar la descarga
+        setLoading(true);
 
         try {
             const videoFormatsRes = await axios.post(`${URLHOST}/api/video-formats`, { videoLink });
-            const resultados = videoFormatsRes.data.formats.formats;
-            const newVideos = []; // Array temporal para almacenar los videos
-            const newAudios = []; // Array temporal para almacenar los audios
+            const { formats } = videoFormatsRes.data.formats;
+            const newMediaOptions = { videos: [], audios: [], iframe: videoFormatsRes.data.formats.videoDetails.embed.iframeUrl };
 
-            resultados.forEach( obj => {
-                let isMp4 = obj.mimeType.includes('mp4');
-                if(obj.hasVideo) {  // Si es Video...
-                    if(isMp4) {
-                    
-                        // Obtener el tamaño del archivo en bytes
-                        const tamañoBytes = parseInt(obj.contentLength);
-                        // Convertir el tamaño a megabytes
-                        const tamañoMB = tamañoBytes / (1024 * 1024);
-                        if(isNaN(tamañoMB)) { return }
-    
-                        // Extraer solo el tipo MIME sin los codecs
-                        const mimeTypeWithoutCodecs = obj.mimeType.split(';')[0].trim();
-    
-                        newVideos.push(`Formato: ${mimeTypeWithoutCodecs}, Calidad: ${obj.qualityLabel}, Tamaño: ${tamañoMB.toFixed(2)}Mb`);
-                    }
+            formats.forEach(obj => {
+                const { mimeType, hasVideo, hasAudio, contentLength, qualityLabel } = obj;
+                const isMp4 = mimeType.includes('mp4');
+                const tamañoBytes = parseInt(contentLength);
+                const tamañoMB = tamañoBytes / (1024 * 1024);
+
+                if (isNaN(tamañoMB)) return;
+
+                if (hasVideo && isMp4) {
+                    const mimeTypeWithoutCodecs = mimeType.split(';')[0].trim();
+                    newMediaOptions.videos.push(`Formato: ${mimeTypeWithoutCodecs}, Calidad: ${qualityLabel}, Tamaño: ${tamañoMB.toFixed(2)}Mb`);
+                } else if(hasAudio){
+                    newMediaOptions.audios.push(`Audio/Mp3 Tamaño: ${tamañoMB.toFixed(2)}Mb`);
                 }
-                else{
-                    // Obtener el tamaño del archivo en bytes
-                    const tamañoBytes = parseInt(obj.contentLength);
-                    // Convertir el tamaño a megabytes
-                    const tamañoMB = tamañoBytes / (1024 * 1024);
-                    if(isNaN(tamañoMB)) { return }
+            });
 
-                    newAudios.push(`Audio/Mp3 Tamaño: ${tamañoMB.toFixed(2)}Mb`);
-                }
-            })
+            setMediaOptions(newMediaOptions);
 
-            setVideos(newVideos); // Establecer el array de videos una vez que se ha completado el proceso de carga
-            setAudios(newAudios); // Establecer el array de audios una vez que se ha completado el proceso de carga
-            setIframe(videoFormatsRes.data.formats.videoDetails.embed.iframeUrl)
             const info = await axios.post(`${URLHOST}/api/video-info`, { videoLink });
             const download = await axios.post(`${URLHOST}/api/download-video`, { videoLink }, { responseType: 'blob' });
-            setTitle(info.data.title);
+
             const blobUrl = window.URL.createObjectURL(new Blob([download.data]));
-            setUrl(blobUrl);
-        } catch(error) {
+            setMediaOptions(prevState => ({ ...prevState, title: info.data.title, url: blobUrl }));
+        } catch (error) {
             console.log(error.response.data.error);
         } finally {
-            setLoading(false); // Ocultar el indicador de carga cuando la descarga haya terminado
+            setLoading(false);
         }
     }
 
-    return ( 
+    return (
         <div className="mt-5 mx-auto d-flex flex-column justify-content-center align-items-center w-50">
             <form onSubmit={handleSubmit}>
                 <label htmlFor="inputUrl">Ingresa la URL del video</label>
-                <input type="text" id="inputUrl" value={videoLink} onChange={handleChange}/>
+                <input type="text" id="inputUrl" value={videoLink} onChange={handleChange} />
                 <button type="submit">Buscar Video</button>
                 <select name="" id="">
-                    {
-                        videos.map((video, index) => (
-                            <option key={index} value="">{video}</option>
-                        ))
-                    }
+                    {mediaOptions.videos.map((video, index) => (
+                        <option key={index} value="">{video}</option>
+                    ))}
                 </select>
                 <select name="" id="">
-                    {
-                        audios.map((audio, index) => (
-                            <option key={index} value="">{audio}</option>
-                        ))
-                    }
+                    {mediaOptions.audios.map((audio, index) => (
+                        <option key={index} value="">{audio}</option>
+                    ))}
                 </select>
             </form>
             {loading ? (
-                <Loading /> // Mostrar el indicador de carga mientras se descarga el archivo
+                <Loading />
             ) : (
-                url && <a className="btn btn-success mt-3 fs-4" href={url} download={`${title}.mp3`}>Guardar</a>
+                mediaOptions.url && <a className="btn btn-success mt-3 fs-4" href={mediaOptions.url} download={`${mediaOptions.title}.mp3`}>Guardar</a>
             )}
-            <iframe className="mt-3" src={iframe} width="800px" height="400px"></iframe>
+            {mediaOptions.iframe && <iframe className="mt-3" src={mediaOptions.iframe} width="800px" height="400px"></iframe>}
         </div>
     );
 }
